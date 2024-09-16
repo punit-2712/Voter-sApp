3f0067db8a26a6061bfbcac43d3f2e846932a5c3
Punit Vachhani <40193957+punit-2712@users.noreply.github.com>
2024-09-16 20:41:51 +0530

    Votes Update

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -75,4 +75,46 @@ router.delete("/:candidateID", jwtAuthMiddleware, async (req, res) => {
     res.status(500).json({ error: "Internal serveer Error" });
   }
 });
+
+
+//voter abhi 
+
+router.post("/vote/:candidateID", jwtAuthMiddleware,async(req,res)=>{
+
+  const candidateId=req.params.candidateID;
+  const userId=req.user.id;
+  try{
+
+    const candidate = await Candidate.findById(candidateId);
+    if (!candidate) {
+      return res.status(404).json({ message: "Candidate not found" });
+    }
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (user.isVoted) {
+      return res.status(400).json({ message: "You have already voted" });
+    }
+    if (user.role == "admin") {
+      return res.status(403).json({ message: "Admin is not allowed" });
+    }
+
+    //update the candidate document after voting 
+
+    candidate.votes.push({user:userId});
+    candidate.voteCount++;
+    await candidate.save();
+
+    //update the user Document 
+    user.isVoted=true;
+    await user.save();
+
+  }catch(err){
+    
+
+  }
+
+
+});
 module.exports = router;
